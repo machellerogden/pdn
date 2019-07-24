0442c1d6f811ae31f996e11724321df00658ff24
Mac Heller-Ogden <mac@machellerogden.com>
2019-07-23 21:06:06 -0500

    beginning on proper tokenizer

--- index.js
+++ index.js
@@ -4,13 +4,20 @@
 const { EOL } = require('os');
 const { nil, Trampoline } = require('./lib/util');
 const { SyntaxError } = require('./lib/errors');
-const { createNameFactory } = require('./lib/names');
 
-function getArgs(arg, type, args) {
-    return !arg.startsWith('@')
-        ? [ arg ]
-        : [ ...args ];
-}
+const primitives = new Set([ 'string', 'number', 'boolean' ]);
+const builtInTypes = {
+    obj(...args) {
+        return args.reduce((acc, v, i, col) => {
+            if (i % 2) acc[col[i - 1]] = v;
+            return acc;
+        }, {});
+    },
+    str(arg) {
+        return arg;
+    }
+
+};
 
 const isDefined = v => v != nil;
 const reserved = new Set([EOL, ',', ' ', '[', ']', '(', ')']);
@@ -21,80 +28,74 @@ const isListStart = v => listStart.has(v);
 const isListEnd = v => listEnd.has(v);
 const isWhitespace = v => /[\s\,]/.test(v);
 
-function Reader() {
-    const NameFactory = createNameFactory(); 
+function tokenize(input) {
+    const raw = Array.isArray(input)
+        ? input.join(' ')
+        : raw;
 
-    const createTokens = Trampoline(function _createTokens(arg) {
-        if (Array.isArray(arg)) return arg.map(createTokens);
-        if (typeof arg === 'object') return arg;
-        const {
-            groups: {
-                type = 'string',
-                raw
-            } = {}
-        } = arg.match(/^@(?<type>[^\/]+)\/?(?<raw>.+)?/) || {};
+    const chars = raw.split('');
+    const result = [];
+    let i = 0;
+    const drop = () => chars.splice(i, 1);
+    const next = () => i++;
+    const stack = [ result ];
+    const current = () => stack[stack.length - 1];
+    const add = (v) => (current().push(v), v);
+    const enter = (v) => (next(), stack.push(add(v)), v);
+    const exit = () => (next(), stack.pop());
 
-        const [
-            name = NameFactory(type).next(),
-            ...args
-        ] = getArgs(arg, type, raw
-            ? raw.split(':')
-            : []);
+    while (i < chars.length) {
 
-        return { name, type, args };
+        if (isListStart(chars[i])) {
+            enter([]);
+            continue;
+        }
+
+        if (isListEnd(chars[i])) {
+            exit();
+            continue;
+        }
+
+        if (isWhitespace(chars[i])) {
+            drop();
+            continue;
+        }
+
+        if (isDefined(chars[i]) && !isReserved(chars[i])) {
+            let value = '';
+            while (isDefined(chars[i]) && !isReserved(chars[i])) value += chars[next()];
+            add(value);
+            continue;
+        }
+
+        add(chars[next()]);
+
+    }
+
+    return result;
+}
+
+function Reader({ types:customTypes = {} } = {}) {
+
+    const types = {
+        ...builtInTypes,
+        ...customTypes
+    };
+
+
+    const parse = Trampoline(function _parse(tokens) {
+        return tokens.reduce((acc, token, i, col) => {
+            if (Array.isArray(token)) return [ ...acc, parse(token) ];
+            const { type, args } = token;
+            return [ ...acc, types[type](...args) ];
+        }, []);
     });
 
     function read(input) {
-        input = Array.isArray(input)
-            ? input
-            : [ input ]
-        const structured = input.reduce((acc, raw) => {
-            const chars = raw.split('');
-            const result = [];
-            let i = 0;
-            const drop = () => chars.splice(i, 1);
-            const next = () => i++;
-            const stack = [ result ];
-            const current = () => stack[stack.length - 1];
-            const add = (v) => (current().push(v), v);
-            const enter = (v) => (next(), stack.push(add(v)), v);
-            const exit = () => (next(), stack.pop());
-
-            while (i < chars.length) {
-
-                if (isListStart(chars[i])) {
-                    enter([]);
-                    continue;
-                }
-
-                if (isListEnd(chars[i])) {
-                    exit();
-                    continue;
-                }
-
-                if (isWhitespace(chars[i])) {
-                    drop();
-                    continue;
-                }
-
-                if (isDefined(chars[i]) && !isReserved(chars[i])) {
-                    let value = '';
-                    while (isDefined(chars[i]) && !isReserved(chars[i])) value += chars[next()];
-                    add(value);
-                    continue;
-                }
-
-                add(chars[next()]);
-
-            }
-
-            return [ ...acc, ...result ];
-        }, []);
-
-        return structured.map(createTokens);
+        return parse(tokenize(input));
     }
 
-    return { read };
+    return read;
 }
 
 module.exports = { Reader };
@@ -111,7 +112,7 @@ if (require.main === module) {
             const args = data != null
                 ? data
                 : process.argv.slice(2).filter(v => /^[^\-]/.test(v));
-            const output = JSON.stringify(Reader().read(args), null, 4);
+            const output = JSON.stringify(Reader()(args), null, 4);
             if (!isCI) await clipboardy.write(output);
             process.stdout.write(output);
         } catch (e) {
