99def7bd9a1b6ba6f986150566473d56dc075ede
Mac Heller-Ogden <mac@machellerogden.com>
2019-07-25 21:12:46 -0500

    all the basic wiring is in place

--- index.js
+++ index.js
@@ -3,22 +3,24 @@
 
 const streamify = require('async-stream-generator');
 const { compile } = require('./lib/compiler');
-const { linebreak, intersperse } = require('./lib/util');
+const { linebreak, intersperse, stringify } = require('./lib/util');
 
 if (require.main === module) {
     if (process.stdin.isTTY) {
         if (process.argv[2] == null) {
             return require('repl').start({
-                input: streamify(linebreak(compile(process.stdin))),
+                input: streamify(linebreak(stringify(compile(process.stdin)))),
                 output: process.stdout
             });
         } else {
-            return streamify(compile(intersperse(' ', process.argv.slice(2)).values())).pipe(process.stdout);
+            // TODO: perf test intersperse vs linebreak
+            //return streamify(compile(intersperse(' ', process.argv.slice(2)).values())).pipe(process.stdout);
+            return streamify(stringify(compile(linebreak(process.argv.slice(2).values())))).pipe(process.stdout);
         }
     }
 
     try {
-        streamify(compile(process.stdin)).pipe(process.stdout);
+        streamify(stringify(compile(process.stdin))).pipe(process.stdout);
     } catch (e) {
         console.log(e && e.stack || e);
     }
