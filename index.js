47903d9059107ecc600d6e34ebea4bcd7b5e3ac6
Mac Heller-Ogden <mac@machellerogden.com>
2019-07-26 17:18:22 -0500

    make better

--- index.js
+++ index.js
@@ -3,30 +3,14 @@
 
 const streamify = require('async-stream-generator');
 const { Compiler } = require('./lib/compiler');
+const { Reader, read } = require('./lib/reader');
+
 const {
     linebreak,
     intersperse,
     stringify,
     pipe
 } = require('./lib/util');
-
-function Reader({ readers } = {}) {
-    const { compile } = Compiler({ readers });
-    function read(data) {
-        const input = data[Symbol.asyncIterator]
-            ? input
-            : Array.isArray(data)
-                ? data.values()
-                : [ data ].values();
-        return compile(linebreak(input));
-    }
-    return { read };
-}
-
-function read(data = '', { readers } = {}) {
-    const { read } = Reader({ readers });
-    return read(data);
-}
 
 module.exports = {
     Reader,
@@ -39,22 +23,8 @@ if (require.main === module) {
     const processInputStream = pipe(compile, stringify, linebreak, streamify);
 
     if (process.stdin.isTTY) {
-        const { EOL } = require('os');
-
         return process.argv[2] == null
-            ? require('repl').start({
-                eval: (cmd, context, filename, callback) => {
-                    async function expand(iter) {
-                        const acc = [];
-                        for await (const v of iter) {
-                            acc.push(v);
-                        }
-                        return { __eval_results__: acc };
-                    }
-                    pipe(read, expand)(cmd).then(value => callback(null, value));
-                },
-                writer: ({ __eval_results__ }) => __eval_results__.map(v => require('util').inspect(v, { depth: null, colors: true })).join(EOL)
-            })
+            ? require('./lib/repl').start()
             : processInputStream(intersperse(' ', process.argv.slice(2)).values()).pipe(process.stdout);
     }
 
