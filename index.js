63448090b509710788d3694dc18ef292839726bd
Mac Heller-Ogden <mac@machellerogden.com>
2019-07-29 08:17:38 -0500

    some cleanup

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const {
     readOne
 } = module.exports = require('./lib/reader');
 
-const { intersperse } = require('./lib/util');
-
 if (require.main === module) {
     return process.stdin.isTTY
         ? process.argv[2] == null
             ? require('./lib/repl').start()
-            : readToStream(intersperse(' ', process.argv.slice(2)).values()).pipe(process.stdout)
+            : readToStream(process.argv.slice(2).join(' ')).pipe(process.stdout)
         : readToStream(process.stdin).pipe(process.stdout);
 }
